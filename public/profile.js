9638dbea715e939d86d8a35af5ae22ddf794c73a
LashayC <cokleylashay@gmail.com>
2022-09-15 18:57:33 -0400

    Updated Profile changes. Fixed logout. Added required

--- public/profile.js
+++ public/profile.js
@@ -1,15 +1,21 @@
-function editAccountInfo(id, currentFirstName, currentLastName, currentEmail) {
-    alert('profile values', id, currentEmail, currentFirstName, currentLastName)
-  let newFirstName = document.querySelector("#newFirstName").value;
-  let newLastName = document.querySelector("#newLastName").value;
-  let newEmail = document.querySelector("#newEmail").value;
+function editAccountInfo(id, currentFirstName, currentLastName, currentEmail, currentUser) {
 
+    // alert('profile values' + id + currentEmail + currentFirstName +  currentLastName)
+
+
+  let newFirstName = document.querySelector("#firstName").value;
+  let newLastName = document.querySelector("#lastName").value;
+  let newEmail = document.querySelector("#email").value;
+  let newUser = document.querySelector("#newUserName").value;
+
+//   alert('profile values the new' + newFirstName + newLastName + newEmail)
 
   let accountBody = {};
   accountBody._id = id;
-  accountBody.firstName = currentFirstName || newFirstName;
-  accountBody.lastName = currentLastName || newLastName;
-  accountBody.email = currentEmail || newEmail;
+  accountBody.firstName = newFirstName || currentFirstName;
+  accountBody.lastName = newLastName || currentLastName;
+  accountBody.email = newEmail || currentEmail;
+  accountBody.username = newUser || currentUser;
 
   fetch("/profileAccountUpdate", {
     method: "put",
@@ -22,6 +28,7 @@ function editAccountInfo(id, currentFirstName, currentLastName, currentEmail) {
       }
     })
     .then((data) => {
+        alert('Changes to profile will show after signout. Use updated username on next signin.')
       window.location.reload(true);
     });
 }
